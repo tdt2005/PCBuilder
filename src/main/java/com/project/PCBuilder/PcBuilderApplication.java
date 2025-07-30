6a724a3c78c8a5d735340b9553b66361b91df7e5
tdt2005 <80700981+tdt2005@users.noreply.github.com>
2025-07-30 12:35:30 +0700

    Add files via upload

--- src/main/java/com/project/PCBuilder/PcBuilderApplication.java
+++ src/main/java/com/project/PCBuilder/PcBuilderApplication.java
@@ -2,11 +2,20 @@ package com.project.PCBuilder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(title = "PC Builder API", version = "1.0", description = "API for PC Builder project"),
+    servers = @Server(url = "https://pcbuilder-production-592f.up.railway.app")
+)
 @SpringBootApplication
 public class PcBuilderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PcBuilderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PcBuilderApplication.class, args);
+    }
 
 }
