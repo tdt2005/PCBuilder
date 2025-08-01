75c865cf42f411d9cb1dd91296c265b8123fda96
tdt2005 <80700981+tdt2005@users.noreply.github.com>
2025-08-01 09:07:28 +0700

    Update PcBuilderApplication.java

--- src/main/java/com/project/PCBuilder/PcBuilderApplication.java
+++ src/main/java/com/project/PCBuilder/PcBuilderApplication.java
@@ -7,7 +7,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-
+@OpenAPIDefinition(
+     info = @Info(title = "PC Builder API", version = "1.0", description = "API for PC Builder project"),
+     servers = @Server(url = "https://pcbuilder-production-592f.up.railway.app")
+)
 @SpringBootApplication
 public class PcBuilderApplication {
 
